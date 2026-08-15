dd6f99b3e7a024ccb32fa41d85491a527e2d5a4b
Wiraaja01 <ocha641@gmail.com>
2026-08-15 21:46:02 +0700

    feat: add dockerfile and github action workflow

---
+++ 11_CICD_Project/1_Github_Action/app.py
@@ -0,0 +1,6 @@
+# app.py
+def main():
+    print("Aplikasi Berhasil Berjalan di Container!")
+
+if __name__ == "__main__":
+    main()
---
+++ README.md
@@ -0,0 +1,3 @@
+# DevOps Learning Journey
+Berisi tentang Materi di DevOps yang sudah saya kuasai
+Telah belajar selama 4 bulan
